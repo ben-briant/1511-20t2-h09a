3df47cd257fadbf2ab6c3c57d6d3eeab24bab5c6
Ben Briant <z5254701@unsw.edu.au>
2020-05-30 11:18:44 +1000

    Added week 1

---
+++ wk01/hello.c
@@ -0,0 +1,20 @@
+// Basic Hello World program
+// Marc Chee, June 2019
+
+#include <stdio.h>
+
+int main (void) {
+    printf("Hello World\n");
+    return 0;
+}
+
+
+/*
+
+1. \n
+
+2. Comments
+
+3. Indentation/whitespace
+
+*/
